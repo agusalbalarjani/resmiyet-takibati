8b2792fb58d2be26e7ab78fb90be65e1d9940656
agusalbalarjani <agus.albalarjani@gmail.com>
2026-09-06 22:09:47 +0700

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'resmiyet-pwa-v2';
+const APP_SHELL = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  const url = new URL(req.url);
+  if (req.method !== 'GET' || url.origin !== self.location.origin || url.pathname.startsWith('/api/')) return;
+
+  if (req.mode === 'navigate') {
+    event.respondWith(fetch(req).catch(() => caches.match('/index.html')));
+    return;
+  }
+
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
+    if (res.ok) {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+    }
+    return res;
+  }).catch(() => cached)));
+});
